e33513abe150500b8039ec31214fca216b3faba7
Vinicius Cãndido <viniciuscandidof320@gmail.com>
2026-06-08 10:54:31 -0300

    Update 07.dart

---
+++ Dart/07.dart
@@ -0,0 +1,13 @@
+List<String> animals = new List<String>();
+  
+void main() {
+  var name = 'Marry'; 
+  var kind = 'cat';
+  
+  //var succeeded = registerAnimal(name, kind);
+  var name2 = 'Fred';
+  var kind2 = 'Dog';
+
+  animals.add(name);
+  
+}
