ccdd1b0a092bf1cbe4f1b4de3ae305f72a206730
Vinicius Cãndido <viniciuscandidof320@gmail.com>
2026-06-11 23:43:00 -0300

    att

--- Dart/07.dart
+++ Dart/07.dart
@@ -1,13 +1,22 @@
-List<String> animals = new List<String>();
+List<String> animals = [];
   
 void main() {
   var name = 'Marry'; 
   var kind = 'cat';
-  
-  //var succeeded = registerAnimal(name, kind);
+  var succeeded = registerAnimal(name, kind);
+  print(succeeded);
   var name2 = 'Fred';
   var kind2 = 'Dog';
-
-  animals.add(name);
-  
+  succeeded = registerAnimal(name2, kind2);
+  print(succeeded);
+  print(animals[0]);
 }
+
+bool registerAnimal(String name, String kind) {
+  animals.add('$name the $kind');
+  return true;
+}
+  
+  
+  
+  
